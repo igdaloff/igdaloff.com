7378f28ec1b7664e834ee4e5891a5b36a47d9755
Nathan Igdaloff <louis.nathan@gmail.com>
2022-03-14 17:36:21 -0700

    get rid of Jekyll for now, don't need it; get some content and style in there

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inconsolata','Helvetica','sans-serif'],
+      serif: ['Jacques Francois', 'Georgia', 'serif']     
+    },
+    colors: {
+      'black': '#222222',
+      'dark-grey': '#303030',
+      'blue': 'rgb(59 130 246)'
+    }
   },
   plugins: [],
+
 }
