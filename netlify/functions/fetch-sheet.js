f81d85eb39a785409b6d3525ba3d22c06d95a612
Nathan Igdaloff <louis.nathan@gmail.com>
2022-04-12 11:26:23 -0700

    netlify function now with my own js

--- netlify/functions/fetch-sheet.js
+++ netlify/functions/fetch-sheet.js
@@ -1,51 +1,46 @@
 
 exports.handler = async (event, context) => {
 
+  console.log(event);
+  const { GoogleSpreadsheet } = require('google-spreadsheet'); // More info https://theoephraim.github.io/node-google-spreadsheet/#/classes/google-spreadsheet
 
+  // const creds = require('../client_secret.json');
+  const doc = new GoogleSpreadsheet('1xBjImwNyfIwpyTS9_scCFO3dWXdn5xeyPYOIDTN8UKs');
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-  // const { GoogleSpreadsheet } = require('google-spreadsheet'); // More info https://theoephraim.github.io/node-google-spreadsheet/#/classes/google-spreadsheet
+  async function accessSpreadsheet(){
+    await doc.useServiceAccountAuth({
+      // client_email: creds.client_email,
+      // private_key: creds.private_key
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY    
+    });
 
-  // // const creds = require('../client_secret.json');
-  // const doc = new GoogleSpreadsheet('1xBjImwNyfIwpyTS9_scCFO3dWXdn5xeyPYOIDTN8UKs');
-
-  // async function accessSpreadsheet(){
-  //   await doc.useServiceAccountAuth({
-  //     // client_email: creds.client_email,
-  //     // private_key: creds.private_key
-  //     client_email: process.env.CLIENT_EMAIL,
-  //     private_key: process.env.PRIVATE_KEY    
-  //   });
-
-  //   await doc.loadInfo(); //Loads document properties and worksheets
-  //   const sheet = doc.sheetsByIndex[0];
+    await doc.loadInfo(); //Loads document properties and worksheets
+    const sheet = doc.sheetsByIndex[0];
     
-  //   const rows = await sheet.getRows();
-  //   const rowsArray = Object.entries(rows); 
+    const rows = await sheet.getRows();
+    const rowsArray = Object.entries(rows); 
 
-  //   let latestArtistName;
-  //   let latestArtistUrl;
+    let latestArtistName;
+    let latestArtistUrl;
 
-  //   //Add spreadsheet data to HTML
-  //   latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
-  //   latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;   
+    //Add spreadsheet data to HTML
+    latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
+    latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;   
 
-  //   // if(sheet){
-  //   //   latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
-  //   //   latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;    
-  //   // } else {
-  //   //   latestArtistName = "Pulp";
-  //   //   latestArtistUrl = "https://open.spotify.com/artist/36E7oYfz3LLRto6l2WmDcD?si=ZIl-fSHBQ_yChwsvEoHvkA";
-  //   // }
+    // if(sheet){
+    //   latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
+    //   latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;    
+    // } else {
+    //   latestArtistName = "Pulp";
+    //   latestArtistUrl = "https://open.spotify.com/artist/36E7oYfz3LLRto6l2WmDcD?si=ZIl-fSHBQ_yChwsvEoHvkA";
+    // }
 
-  //   document.querySelector('.listening-to').innerHTML = latestArtistName;
-  //   document.querySelector('.listening-to').href = latestArtistUrl;    
-  // }
+    document.querySelector('.listening-to').innerHTML = latestArtistName;
+    document.querySelector('.listening-to').href = latestArtistUrl;    
+  }
 
-  // accessSpreadsheet();
+  accessSpreadsheet();
 
 }
 
