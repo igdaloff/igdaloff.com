0355f67b0a95f9ebeddce78914cf4d384942a86a
Nathan Igdaloff <louis.nathan@gmail.com>
2022-04-11 17:43:17 -0700

    set up secret keys with netlify; testing them - still need to cleanup

---
+++ functions/fetch-sheet/fetch-sheet.js
@@ -0,0 +1,7 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event) => {
+  const PRIVATE_KEY = " "
+  const CLIENT_EMAIL = " "
+}
+
+module.exports = { handler }
--- src/index.js
+++ src/index.js
@@ -3,24 +3,39 @@ const { GoogleSpreadsheet } = require('google-spreadsheet'); // More info https:
 const creds = require('../client_secret.json');
 const doc = new GoogleSpreadsheet('1xBjImwNyfIwpyTS9_scCFO3dWXdn5xeyPYOIDTN8UKs');
 
+
+// fetch(`/.netlify/functions/fetch-sheet?name="Bob"`)
+//   .then(res => {console.log(response.json())})
+
+
 async function accessSpreadsheet(){
   await doc.useServiceAccountAuth({
-    client_email: creds.client_email,
-    private_key: creds.private_key,
+    // client_email: creds.client_email,
+    // private_key: creds.private_key
+    client_email: CLIENT_EMAIL,
+    private_key: PRIVATE_KEY    
   });
 
   await doc.loadInfo(); //Loads document properties and worksheets
 	const sheet = doc.sheetsByIndex[0];
 	
   const rows = await sheet.getRows();
-  const rowsArray = Object.entries(rows);  
+  const rowsArray = Object.entries(rows); 
 
-  const latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
-  const latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;
+  let latestArtistName;
+  let latestArtistUrl;
 
   //Add spreadsheet data to HTML
+  if(sheet){
+    latestArtistName = rowsArray[rowsArray.length - 1][1].Artist;
+    latestArtistUrl = rowsArray[rowsArray.length - 1][1].SpotifyUrl;    
+  } else {
+    latestArtistName = "Pulp";
+    latestArtistUrl = "https://open.spotify.com/artist/36E7oYfz3LLRto6l2WmDcD?si=ZIl-fSHBQ_yChwsvEoHvkA";
+  }
+
   document.querySelector('.listening-to').innerHTML = latestArtistName;
-  document.querySelector('.listening-to').href = latestArtistUrl;
+  document.querySelector('.listening-to').href = latestArtistUrl;    
 }
 
 accessSpreadsheet();
