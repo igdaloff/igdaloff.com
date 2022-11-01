1a5640e23ea389eb482e991f293fac52f2f7fc3f
Nathan Igdaloff <louis.nathan@gmail.com>
2022-11-01 11:38:56 -0700

    in progress dark mode messing around

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,38 @@
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ['*.{html,js}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontWeight: {
-        'normal': '300'
+        normal: '300',
       },
       colors: {
-        'black': '#222222',
+        black: '#222222',
         'dark-grey': '#303030',
-        'blue': '#2357cd',
-        'tan': '#f8f5ee',
-        'dark-green': '#0f3602'
+        blue: '#2357cd',
+        tan: '#f8f5ee',
+        'dark-green': '#0f3602',
+        'neon-green': '#0FFF50',
       },
       letterSpacing: {
-        'wide': '0.015em'
-      }
+        wide: '0.015em',
+      },
     },
     fontFamily: {
-      sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],      
-      title: ['Inconsolata','Helvetica','sans-serif'],
-      mono: ['Inconsolata', 'ui-monospace']
-    }
-  }
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Open Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+      title: ['Inconsolata', 'Helvetica', 'sans-serif'],
+      mono: ['Inconsolata', 'ui-monospace'],
+    },
+  },
 }
