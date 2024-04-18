173f219340441b3b4daef0a30ec7a33997e82f5c
igdaloff <louis.nathan@gmail.com>
2024-04-18 10:57:41 -0700

    adjust OFF tilt on page load

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## To start 'er up
 
 1. npm run dev
-2. five-server
+2. watchify src/index.js -o dist/compiled.js
+3. five-server
 
 More info on google-spreadsheet wrapper used to get latest music: https://theoephraim.github.io/node-google-spreadsheet/#/classes/google-spreadsheet
--- src/index.js
+++ src/index.js
@@ -4,39 +4,19 @@ const axios = require('axios');
 axios
   .get('/.netlify/functions/fetch-sheet')
   .then(function (response) {
-    // handle success
     document.querySelector('.listening-to').innerHTML = response.data.artistName;
     document.querySelector('.listening-to').href = response.data.artistUrl;
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   });
 
-//Set 'off' rotation based on cursor Y position
-const pos = { y: 0 };
-
-const saveCursorPosition = function (y) {
-  pos.y = (y / window.innerHeight).toFixed(2) * 90 + 'deg';
-  document.documentElement.style.setProperty('--y', pos.y);
-};
-
-document.addEventListener('mousemove', e => {
-  saveCursorPosition(e.clientY);
-});
-
-//Set 'off' rotation to 0 if mouse leaves window so as to not be annoying
-document.addEventListener('mouseleave', e => {
-  if (e.clientY <= 0 || e.clientX <= 0 || e.clientX >= window.innerWidth || e.clientY >= window.innerHeight) {
-    saveCursorPosition(0);
-  }
-});
-
 window.onload = function () {
-  //set body class to 'loaded' after 1s to trigger CSS animations
+  const offText = document.querySelector('h1 span');
   setTimeout(function () {
-    document.body.classList.add('loaded');
-  }, 1000);
+    offText.classList.add('un-rotate');
+  }, 1500);
+
   //Detect if tab is active to change favicon accordingly
   document.addEventListener('visibilitychange', function () {
     const isPageActive = !document.hidden;
