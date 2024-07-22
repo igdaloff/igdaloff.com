5b0992df573acb74211a3c31396b0b1291876b9a
Nathan Igdaloff <louis.nathan@gmail.com>
2024-07-22 11:35:49 -0700

    Saved track player (#2)
    * add back missing icons
    
    * use concurrently to simplify watch/serve process
    
    * build the player plus some other cleanup
    
    * try new player design
    
    * finalize player; fix typo
    
    * adjust bottom spacing
    
    * fix white bg
    
    * prevent player from appearing too early
    
    * player bounce back animation

--- dist/compiled.js
+++ dist/compiled.js
@@ -1091,7 +1091,7 @@ utils.forEach(['post', 'put', 'patch'], function forEachMethodWithData(method) {
 module.exports = defaults;
 
 }).call(this)}).call(this,require('_process'))
-},{"../adapters/http":2,"../adapters/xhr":2,"../core/enhanceError":12,"../helpers/normalizeHeaderName":26,"../utils":30,"./transitional":17,"_process":32}],17:[function(require,module,exports){
+},{"../adapters/http":2,"../adapters/xhr":2,"../core/enhanceError":12,"../helpers/normalizeHeaderName":26,"../utils":30,"./transitional":17,"_process":31}],17:[function(require,module,exports){
 'use strict';
 
 module.exports = {
@@ -1895,39 +1895,6 @@ module.exports = {
 };
 
 },{"./helpers/bind":19}],31:[function(require,module,exports){
-const axios = require('axios');
-
-//Get recent Spotify liked artists from spreadsheet
-axios
-  .get('/.netlify/functions/fetch-sheet')
-  .then(function (response) {
-    document.querySelector('.listening-to').innerHTML = response.data.artistName;
-    document.querySelector('.listening-to').href = response.data.artistUrl;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-window.onload = function () {
-  const offText = document.querySelector('h1 span');
-  setTimeout(function () {
-    offText.classList.add('un-rotate');
-  }, 1500);
-
-  //Detect if tab is active to change favicon accordingly
-  document.addEventListener('visibilitychange', function () {
-    const isPageActive = !document.hidden;
-    const favicon = document.querySelector('[rel=icon]');
-
-    if (!isPageActive) {
-      favicon.href = './src/images/favicon/off/favicon-32x32.png';
-    } else {
-      favicon.href = './src/images/favicon/favicon-32x32.png';
-    }
-  });
-};
-
-},{"axios":1}],32:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
@@ -2113,4 +2080,87 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
-},{}]},{},[31]);
+},{}],32:[function(require,module,exports){
+const axios = require('axios');
+
+//Get recent Spotify liked artists from spreadsheet
+axios
+  .get('/.netlify/functions/fetch-sheet')
+  .then(function (response) {
+    const artistName = response.data.artistName;
+    const trackUrl = response.data.trackUrl;
+    const artistImage = response.data.artistImage.split(',')[0];
+    const trackName = response.data.trackName;
+    const dateAdded = response.data.dateAdded;
+    let dateAddedFormatted = new Date(dateAdded).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+      timeZoneName: 'short'
+    });
+
+    // If date is today, return "Today" instead of the date. If date is yesterday, return "Yesterday" instead of the date.
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    let isToday = false;
+    let isYesterday = false;
+
+    if (new Date(dateAdded).toDateString() === today.toDateString()) {
+      isToday = true;
+    } else if (new Date(dateAdded).toDateString() === yesterday.toDateString()) {
+      isYesterday = true;
+    }
+
+    const html = String.raw;
+    const playerTemplate = html`
+      <div class="player text-sm sm:w-[300px] border border-black border-solid shadow-[4px_4px_5px_rgba(0,0,0,0.25)]">
+        <div class="player-inner p-1 flex gap-2 items-center justify-start">
+          <a href="${trackUrl}" class="size-12">
+            <img class="block w-full rounded-sm" src="${artistImage}" alt="Image of ${artistName} album art" />
+          </a>
+          <a href="${trackUrl}" class="flex no-underline flex-col max-w-[60%]">
+            <span class="font-bold text-nowrap text-ellipsis overflow-hidden sm:hover:underline">${artistName}</span>
+            <span class="text-nowrap text-ellipsis overflow-hidden sm:hover:underline">${trackName}</span>
+          </a>
+          <a
+            href="${trackUrl}"
+            class="ml-auto text-black sm:hover:text-white sm:hover:bg-black mr-1 border border-black rounded-full size-8 flex justify-center items-center">
+            <span class="play-triangle relative right-[-2px]"></span>
+          </a>
+        </div>
+        <div class="p-1 text-xs flex gap-1 items-center border-t-black border-t">
+          <span class=" italic before:content-['>'] before:not-italic">
+            Last track saved ${!!isToday ? 'today' : ''} ${!!isYesterday ? 'yesterday' : ''}
+            ${!isToday & !isYesterday ? dateAddedFormatted : ''}
+          </span>
+        </div>
+      </div>
+    `;
+
+    const playerContainer = document.querySelector('.track-player');
+    playerContainer.innerHTML = playerTemplate;
+    playerContainer.classList.add('animate-slide-in');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+window.onload = function () {
+  //Detect if tab is active to change favicon accordingly
+  document.addEventListener('visibilitychange', function () {
+    const isPageActive = !document.hidden;
+    const favicon = document.querySelector('[rel=icon]');
+
+    if (!isPageActive) {
+      favicon.href = './src/images/favicon/off/favicon-32x32.png';
+    } else {
+      favicon.href = './src/images/favicon/favicon-32x32.png';
+    }
+  });
+};
+
+},{"axios":1}]},{},[32]);
